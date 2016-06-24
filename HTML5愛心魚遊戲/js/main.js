59dc8e400b44f9e047a428ebad5405d38ad3037e
SilenceIG726 <jizhengting442@gmail.com>
2016-06-24 14:15:36 +0800

    add background and anemone

---
+++ HTML5愛心魚遊戲/js/main.js
@@ -0,0 +1,46 @@
+var can1;
+var can2;
+
+var canWidth;
+var canHeight;
+
+var ctx1;
+var ctx2;
+
+var lastTime;
+var deltaTime;
+
+var bgPic = new Image();
+
+var ane;
+document.body.onload=game;
+function game() {
+	init();
+	lastTime = Date.now();
+	deltaTime = 0;
+	gameloop();
+}
+function init() {
+	//獲得canvas context
+	can1 = document.getElementById('canvas1');//fishes, dust, UI, circle
+	ctx1 = can1.getContext('2d');
+	can2 = document.getElementById('canvas2');//background, ane, fruits
+	ctx2 = can2.getContext('2d');
+
+	bgPic.src = "./src/background.jpg";
+
+	canWidth = can1.width;
+	canHeight = can1.height;
+
+	ane = new aneObj();
+	ane.init();
+}
+function gameloop() {
+	requestAnimFrame(gameloop);//setInterval, setTimeout, frame per second
+	var now = Date.now();
+	deltaTime = now - lastTime;
+	lastTime = now;
+
+	drawBackground();
+	ane.draw();
+}
